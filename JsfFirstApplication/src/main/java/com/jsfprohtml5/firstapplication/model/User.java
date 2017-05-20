b9162ceff620bfeebb6dec4bc9f6715b0d1a80f1
Robert Takacs <robino13@gmail.com>
2017-05-20 17:39:13 +0200

    First JSF Application for Glassfish

---
+++ JsfFirstApplication/src/main/java/com/jsfprohtml5/firstapplication/model/User.java
@@ -0,0 +1,28 @@
+package com.jsfprohtml5.firstapplication.model;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class User implements Serializable {
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
